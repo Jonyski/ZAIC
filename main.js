1bd75b30b51c31643781c40a7640a79d2e4b5ec3
Jonyski <geleiaeavela@gmail.com>
2023-04-06 20:17:45 -0300

    adds playPiece mechanic

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ console.log(board);
 
 /*
 Now i guess it's a good time to initialize some of the variables that will
-be used on the entire program
+be used on the entire program, and also create the players
 */
 
 let currentTurn = 0;
@@ -29,39 +29,40 @@ let playedPieces = [];
 let players = [];
 
 class Player {
-	constructor(playerNum){
+	constructor(playerNum, colors){
 		this.number = playerNum;
 		this.selectedPiece = "big-piece";
 		this.bigPieces = 8;
 		this.longPieces = 8;
 		this.smallPieces = 3;
 		this.points = 0;
-		this.colors = ["TODO..."]
+		this.colors = colors
 	}
 
-	playPiece(){
-		switch(this.selectedPiece){
-		case "big-piece":
-			this.bigPieces -= 1;
-			document.querySelector(`.big-piece .P${this.number}-piece`).innerHTML = this.bigPieces;
-		case "horizontal-long-piece":
-		case "vertical-long-piece":
-			this.longPieces -= 1;
-			document.querySelector(`.horizontal-long-piece .P${this.number}-piece`).innerHTML = this.longPieces;
-			document.querySelector(`.vertical-long-piece .P${this.number}-piece`).innerHTML = this.longPieces;
-		case "small-piece":
-			this.smallPieces -= 1;
-			document.querySelector(`.small-piece .P${this.number}-piece`).innerHTML = this.smallPieces;
-		}
-	}
+	// playPiece(){
+	// 	switch(this.selectedPiece){
+	// 	case "big-piece":
+	// 		this.bigPieces -= 1;
+	// 		document.querySelector(`.big-piece .P${this.number}-piece`).innerHTML = this.bigPieces;
+	// 	case "horizontal-long-piece":
+	// 	case "vertical-long-piece":
+	// 		this.longPieces -= 1;
+	// 		document.querySelector(`.horizontal-long-piece .P${this.number}-piece`).innerHTML = this.longPieces;
+	// 		document.querySelector(`.vertical-long-piece .P${this.number}-piece`).innerHTML = this.longPieces;
+	// 	case "small-piece":
+	// 		this.smallPieces -= 1;
+	// 		document.querySelector(`.small-piece .P${this.number}-piece`).innerHTML = this.smallPieces;
+	// 	}
+	// }
 }
 
-let P1 = new Player(1);
-let P2 = new Player(2);
+let P1 = new Player(1, ["#f7d040"]);
+let P2 = new Player(2, ["#3b97ed"]);
 
 players.push(P1);
 players.push(P2);
 
+let currentPlayer = P1;
 /* 
 now we initialize the events, they are:
 - selecting a piece
@@ -107,7 +108,54 @@ startStopButton.addEventListener("click", e => {
 	e.target.classList.toggle("start");
 });
 
+let cells = document.getElementsByClassName("cell");
+
+for(let i = 0; i < cells.length; i++){
+	cells[i].addEventListener("click", e => {
+		playPiece(e.target.dataset.x, e.target.dataset.y, currentPlayer.selectedPiece);
+
+		//switches the current player (ternary op)
+		currentPlayer = currentPlayer == P1 ? P2 : P1;
+		currentTurn++;
+		console.log(currentPlayer)
+	})
+}
+
+// GAME DEFINING FUNCTIONS
+
+//funcion isValidPlacing(x, y, pieceType){ TODO }
+
+function playPiece(x, y, pieceType){
+	//find index of the cell in the board array
+	let coords = getIndexesOfCellByXY(x, y);
+
+	board[coords.y][coords.x].style.background = currentPlayer.colors[0];
+
+	//TODO: change element dataset.owner
+
+	if(currentPlayer.selectedPiece == "big-piece"){
+		board[coords.y + 1][coords.x].style.background = currentPlayer.colors[0];
+		board[coords.y][coords.x + 1].style.background = currentPlayer.colors[0];
+		board[coords.y + 1][coords.x + 1].style.background = currentPlayer.colors[0];
+	} else if(currentPlayer.selectedPiece == "horizontal-long-piece"){
+		board[coords.y][coords.x + 1].style.background = currentPlayer.colors[0];
+	} else if(currentPlayer.selectedPiece == "vertical-long-piece"){
+		board[coords.y + 1][coords.x].style.background = currentPlayer.colors[0];
+	}
+
+}
+
 // UTILITY FUNCTIONS
+function getIndexesOfCellByXY(x, y){
+	for(let i = 0; i < board.length; i++){
+		for(let j = 0; j < board[i].length; j++){
+			if(board[i][j].dataset.x == x && board[i][j].dataset.y == y){
+				return {y: i, x: j};
+			}
+		}
+	}
+	console.log("indexes of the cell could not be found");
+}
 
 function removeColumn(columnIndex){
 	let columnLen = board.length;
